82d7b0a530cf322ab8f3def3f0121eb980cbe98a
NobutakaSuzuki <suzuki_n@opening-line.co.jp>
2024-11-04 20:39:01 +0900

    購入する商品数を昇降させるReducerを作成

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { PlusIcon, MinusIcon } from '../HeroIcons'
 import { useDispatch } from 'react-redux';
-import { removeItem } from '../features/cart/CartSlice';
+import { removeItem, increase, decrease } from '../features/cart/CartSlice';
 
 const CartItem = ({ id, img, title, price, amount }) => {
   const  dispatch = useDispatch();
@@ -14,11 +14,18 @@ const CartItem = ({ id, img, title, price, amount }) => {
         <button className='remove-btn' onClick={() => dispatch(removeItem(id))}>削除</button>
       </div>
       <div>
-        <button className='amount-btn'>
+        <button className='amount-btn' onClick={() => dispatch(increase(id))}>
           <PlusIcon/>
         </button>
         <p className='amount'>{amount}</p>
-        <button className='amount-btn'>
+        <button className='amount-btn' onClick={() => {
+          if (amount === 1) {
+            dispatch(removeItem(id));
+            return;
+          } else {
+            dispatch(decrease(id));
+          }
+        }}>
           <MinusIcon/>
         </button>
       </div>
